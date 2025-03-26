5d0a2bce1030cc46ebdb495984f2f59e7058e95a
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-26 13:40:03 +0100

    Movie and Song classes

--- best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/classes/Song.java
+++ best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/classes/Song.java
@@ -1,5 +1,41 @@
 package org.lessons.java.spring.best_of_the_year.classes;
 
-public class Song {
+import java.io.Serializable;
 
+public class Song implements Serializable {
+    // variabili
+    private int id;
+    private String titolo;
+
+    // costruttore vuoto
+    public Song() {
+    }
+
+    // costruttore con parametri
+    public Song(int id, String titolo) {
+        this.id = id;
+        this.titolo = titolo;
+    }
+
+    // getter e setter
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return this.titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %s", id, titolo);
+    }
 }
