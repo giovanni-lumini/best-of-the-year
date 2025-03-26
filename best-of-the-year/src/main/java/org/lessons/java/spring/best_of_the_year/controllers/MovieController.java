41ca1be6db01468f7626cf109d0a0fd54e047394
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-26 13:56:56 +0100

    Movie and Song controllers

--- best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/MovieController.java
+++ best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/MovieController.java
@@ -1,5 +1,35 @@
 package org.lessons.java.spring.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+
+import org.lessons.java.spring.best_of_the_year.classes.Movie;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movies")
+
 public class MovieController {
 
+    @GetMapping("/movies")
+    /* http://localhost:8080/movies */
+    public String movies(Model model) {
+        // creo nuovi movies
+        Movie spidermanUno = new Movie(1, "Spiderman 1");
+        Movie spidermanDue = new Movie(2, "Spiderman 2");
+        Movie spidermanTre = new Movie(3, "Spiderman 3");
+
+        // creo un ArrayList
+        ArrayList<Movie> movies = new ArrayList<>();
+        // aggiungo i nuovi movies all'ArrayList
+        movies.add(spidermanUno);
+        movies.add(spidermanDue);
+        movies.add(spidermanTre);
+
+        model.addAttribute("movies", movies);
+
+        return "movie";
+    }
 }
--- best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/SongController.java
+++ best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/SongController.java
@@ -1,5 +1,35 @@
 package org.lessons.java.spring.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+
+import org.lessons.java.spring.best_of_the_year.classes.Song;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/songs")
+
 public class SongController {
 
+    @GetMapping("/songs")
+    /* http://localhost:8080/songs */
+    public String songs(Model model) {
+        // creo nuovi movies
+        Song battito = new Song(1, "Battito");
+        Song columbia = new Song(2, "Columbia");
+        Song karma = new Song(3, "Karma");
+
+        // creo un ArrayList
+        ArrayList<Song> songs = new ArrayList<>();
+        // aggiungo i nuovi movies all'ArrayList
+        songs.add(battito);
+        songs.add(columbia);
+        songs.add(karma);
+
+        model.addAttribute("songs", songs);
+
+        return "song";
+    }
 }
