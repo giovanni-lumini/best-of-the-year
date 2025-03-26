8ca1be5c6789583d3180ef23b23e863f1d62c506
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-26 13:30:39 +0100

    page home

--- best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/HomeController.java
+++ best-of-the-year/src/main/java/org/lessons/java/spring/best_of_the_year/controllers/HomeController.java
@@ -1,5 +1,20 @@
 package org.lessons.java.spring.best_of_the_year.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+
 public class HomeController {
 
+    @GetMapping("/")
+    /* http://localhost:8080/?name=Giovanni */
+    public String home(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
 }
